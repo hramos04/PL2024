914ba3213d7734ffb15293eab8b412bd9bbd90c4
hramos04 <116158805+hramos04@users.noreply.github.com>
2024-02-20 22:12:19 +0000

    TPC1 completo

---
+++ TPC1/README.md
@@ -0,0 +1,14 @@
+# TPC1: Análise de um dataset
+
+## Aluno
+
+**Nome:** Hugo António Gomes Ramos
+
+**Número:** A100644
+
+## Resumo
+O objetivo deste tpc é fazer parsing dos dados de um ficheiro **.csv** e com os dados fazer os seguintes exercícios:
+
+- Lista ordenada alfabeticamente das modalidades desportivas;
+- Percentagens de atletas aptos e inaptos para a prática desportiva;
+- Distribuição de atletas por escalão etário.
--- TPC1/trabalho1.py
+++ TPC1/trabalho1.py
@@ -6,8 +6,7 @@ def listaModalidades(dados):
     for linha in dados.values():
         listaModalidades.add(linha[1])
         
-        
-    return sorted(listaModalidades,key=lambda x: x.lower())
+    return sorted(listaModalidades, key=lambda x: x.lower())
 
 
 def atletasAptos(dados): 
@@ -19,7 +18,20 @@ def atletasAptos(dados):
                          
     return atletasAptos
 
+
+def distribuirPorIdades(dados):
+    escaloes = {"0-5": 0, "6-10": 0, "11-15": 0, "16-20": 0, "21-25": 0, "26-30": 0, "31-35": 0, "36-40": 0, "41-45": 0}
     
+    for linha in dados.values():
+        idade = int(linha[0])
+        for intervalo in escaloes:
+            inicio, fim = map(int, intervalo.split("-"))
+            if inicio <= idade <= fim:
+                escaloes[intervalo] += 1
+                break
+    
+    return escaloes
+
 
 def main():
     
@@ -27,16 +39,21 @@ def main():
     input()
     for linha in sys.stdin:
         split = linha.split(",")
-        dados[split[0]] = [split[5],split[8],split[12]]
+        dados[split[0]] = [split[5], split[8], split[12]]
     
     print(f"Lista modalidades: {listaModalidades(dados)}")
+    
     aptos = atletasAptos(dados)
     total = len(dados)
-    percentagemAptos = aptos/total*100
+    percentagemAptos = aptos / total * 100
     print(f"Percentagem atletas aptos: {percentagemAptos:.2f}%")
-    print(f"Percentagem atletas nao aptos: {100-percentagemAptos:.2f}%")
+    print(f"Percentagem atletas nao aptos: {100 - percentagemAptos:.2f}%")
     
+    distribuicao = distribuirPorIdades(dados)
+    print("Distribuição por escalão etário:")
+    for intervalo, totalAtletas in distribuicao.items():
+        print(f"{intervalo}: {totalAtletas} atletas")
     
+
 if __name__ == "__main__":  
     main()
-    
